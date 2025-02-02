ac00c220342fb0eb84c0b4befacc1aee5b4cac92
luisduarterx <luiscdradm@Gmail.com>
2025-02-01 23:24:39 -0300

    iniciando Auth

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -6,34 +6,75 @@ import "./../painel.css";
 import { Navigation } from "@/components/Navigation";
 import { HeaderPainel } from "@/components/HeaderPainel";
 import { MainPainel } from "@/components/MainPainel";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
+  //transformei em async para aguardar verifytoken
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+
+  const verifyToken = async () => {
+    console.log("acessei");
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      try {
+        const res = await fetch("http://localhost:4000/authenticate", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const json = await res.json();
+
+        if (!json.acess) {
+          console.log(json);
+          return false;
+        }
+        return true;
+      } catch (error) {}
+    } else {
+      router.push("/login");
+      return false;
+    }
+  };
+  //verifica se existe um token
+  const isAuth = verifyToken();
+  if (!isAuth) {
+    router.push("/login");
+    return;
+  }
+
   const [showMenu, setShowMenu] = useState(true);
   const styleShowMenu = {
     marginLeft: showMenu ? "300px" : "0",
     width: showMenu ? "calc(100% - 300px)" : "100%",
   };
   return (
-    <html lang="pt-BR">
-      <head></head>
-      <body>
-        <div className="painel">
-          <Navigation menuOpen={showMenu} setMenu={setShowMenu}></Navigation>
+    (await isAuth) && (
+      //condicional para renderização
+      <html lang="pt-BR">
+        <head></head>
+        <body>
+          <div className="painel">
+            <Navigation menuOpen={showMenu} setMenu={setShowMenu}></Navigation>
 
-          <div className="containerPainel" style={styleShowMenu}>
-            <HeaderPainel
-              menuOpen={showMenu}
-              setMenu={setShowMenu}
-            ></HeaderPainel>
-            <MainPainel>{children}</MainPainel>
+            <div className="containerPainel" style={styleShowMenu}>
+              <HeaderPainel
+                menuOpen={showMenu}
+                setMenu={setShowMenu}
+              ></HeaderPainel>
+              <MainPainel>{children}</MainPainel>
+            </div>
           </div>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
+    )
   );
 }
--- src/components/FormLogin.tsx
+++ src/components/FormLogin.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { redirect, useRouter } from "next/navigation";
+import { stringify } from "querystring";
+import { useEffect, useState } from "react";
 type Props = {
   onSwitch: () => void;
 };
@@ -8,9 +9,43 @@ export const FormLogin = ({ onSwitch }: Props) => {
   const [inputEmail, setEmail] = useState("");
   const [inputPassword, setInputPassword] = useState("");
 
+  const createSession = (token: string) => {
+    console.log(token);
+    localStorage.setItem("token", token);
+    console.log("token criado e registrado");
+    router.push("/app/home");
+  };
+
   const router = useRouter();
-  const handleClick = () => {
-    router.push("/home");
+  const handleClick = async () => {
+    let credenciais = {
+      email: inputEmail.toLowerCase(),
+      senha: inputPassword,
+    };
+    try {
+      const res = await fetch("http://localhost:4000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credenciais),
+      });
+
+      if (res.ok) {
+        const json = await res.json();
+        console.log(json);
+
+        if (json.error) {
+          return console.log(json.error);
+        } else {
+          return createSession(json.token);
+        }
+      } else {
+        alert("ERRO");
+      }
+    } catch (error) {
+      alert("isso ta aparecendo porque");
+    }
   };
 
   return (
