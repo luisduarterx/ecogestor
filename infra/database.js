434981734538a11a4571c76187d91e06fca2a878
Luis Duarte <luiscdradm@gmail.com>
2025-12-30 21:22:02 -0300

    inicia construção da infra

---
+++ infra/database.js
@@ -0,0 +1,22 @@
+import { Client } from "pg";
+async function query(queryObject) {
+  let client;
+  try {
+    client = new Client({
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+    });
+    await client.connect();
+
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+export default { query: query };
--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,5 +1,24 @@
-const status = (req, res) => {
-  res.json({ ok: "ok" });
+import database from "../../../infra/database";
+const status = async (req, res) => {
+  const query_server_version = await database.query("SHOW server_version;");
+  const query_max_connections = await database.query("SHOW max_connections;");
+  const query_openned_conections = await database.query(
+    "SELECT count(*) FROM pg_stat_activity;",
+  );
+
+  const max_connections = Number(query_max_connections.rows[0].max_connections);
+  const openned_connections = Number(query_openned_conections.rows[0].count);
+  const server_version = Number(query_server_version.rows[0].server_version);
+  const result = await database.query("SELECT 1+1;");
+  console.log(result);
+
+  res.json({
+    database: {
+      server_version: server_version,
+      max_connections: max_connections,
+      openned_connections: openned_connections,
+    },
+  });
 };
 
 export default status;
