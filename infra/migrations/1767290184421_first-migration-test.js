a4d8c27931060d41e579503ccdbce486b568908e
Luis Duarte <luiscdradm@gmail.com>
2026-01-02 00:22:03 -0300

    altera migration para commonJS

--- infra/migrations/1767290184421_first-migration-test.js
+++ infra/migrations/1767290184421_first-migration-test.js
@@ -1,18 +1,7 @@
-/**
- * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
- */
-export const shorthands = undefined;
-
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
-export const up = (pgm) => {};
-
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
-export const down = (pgm) => {};
+module.exports.shorthands = undefined;
+module.exports.up = function (pgm) {
+  // UP
+};
+module.exports.down = function (pgm) {
+  // DOWN
+};
