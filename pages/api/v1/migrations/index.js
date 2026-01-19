5478dfb08c6cb5e9c8af164b2e750a729e7e1b7a
Luis Duarte <luiscdradm@gmail.com>
2026-01-19 20:03:49 -0300

    cria primeiro model migrator

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,57 +2,23 @@ import { runner } from "node-pg-migrate";
 import { join } from "path";
 import database from "infra/database.js";
 import { createRouter } from "next-connect";
+import migrator from "models/migrator";
 
 import controller from "infra/controller";
 const router = createRouter();
 
 router.get(async (req, res) => {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    const defaultMigrationsOptions = {
-      dbClient,
-      dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    };
+  const pendingMigrations = await migrator.listPendingMigrations();
 
-    const pendingMigrations = await runner({
-      ...defaultMigrationsOptions,
-      dryRun: true,
-    });
-
-    return res.status(200).json(pendingMigrations);
-  } finally {
-    await dbClient.end();
-  }
+  return res.status(200).json(pendingMigrations);
 });
 router.post(async (req, res) => {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    const defaultMigrationsOptions = {
-      dbClient,
-      dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    };
+  const migratedMigrations = await migrator.runPendingMigrations();
 
-    const migratedMigrations = await runner({
-      ...defaultMigrationsOptions,
-    });
-
-    if (migratedMigrations.length > 0) {
-      return res.status(201).json(migratedMigrations);
-    }
-    return res.status(200).json(migratedMigrations);
-  } finally {
-    await dbClient.end();
+  if (migratedMigrations.length > 0) {
+    return res.status(201).json(migratedMigrations);
   }
+  return res.status(200).json(migratedMigrations);
 });
 
 export default router.handler({
