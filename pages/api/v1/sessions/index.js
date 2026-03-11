288c957e5d326bdbca49f3ecf395bd9fa70fbcb1
Luis Duarte <luiscdradm@gmail.com>
2026-03-11 16:30:49 -0300

    cria model sessions e api/v1/sessions

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -1,4 +1,5 @@
 import { createRouter } from "next-connect";
+import * as cookie from "cookie";
 import controller from "infra/controller";
 import autentication from "models/authentication";
 import session from "models/session";
@@ -12,9 +13,14 @@ router.post(async (req, res) => {
   );
 
   const newSession = await session.create(usuario.id);
-
+  const setCookie = cookie.serialize("sid", newSession.token, {
+    path: "/",
+    maxAge: session.expiration_in_miliseconds / 1000,
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+  });
   console.log(newSession);
-
+  res.setHeader("Set-Cookie", setCookie);
   res.status(201).json(newSession);
 });
 export default router.handler({
